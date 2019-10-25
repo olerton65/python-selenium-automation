72b7e31804b2dda7a32188e8caf44e1ca485b70d
olerton65 <55895616+olerton65@users.noreply.github.com>
2019-10-24 21:23:03 -0700

    Add files via upload
    Homework 3 (part 3)

---
+++ css_selector.py
@@ -0,0 +1,23 @@
+#CSS OR MOST OPTIMAL LOCATORS
+
+1. Amazon Logo: css="div.a-spacing-none i" or "i.a-icon-logo"
+
+2. Create account: css="h1.a-spacing-small"
+
+3. Your name: css="input#ap_customer_name"
+
+4. Email: css="input#ap_email"
+
+5. Password: css="input#ap_password
+
+6. Password must be at least 6 charactes: Sorry, I was not able to find the locator. Maybe this way - Sx="//div[text()='Password must be at least 6 characters']"
+
+7. Re-enter password: css="input#ap_password_check"
+
+8. Create your Amazon acc: css="input.a-button-input[type='submit']"
+
+9.  Conditions of Use: css="a[href*='Conditions of Use']" # Sorry, I saw it in the Lesson 4
+
+10. Privacy Notice: css="a[href*='notification_privacy']"
+
+11. Sign In: css="a[href*='signin']"
